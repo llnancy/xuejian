f58d2ae2f5971d42a3dcf0e04f19edf05e2c7c9e
lilu <admin@lilu.org.cn>
2022-09-26 20:46:27 +0800

    [docs] Update chunyu-helloworld tutorial

--- chunyu-hello-world/src/main/java/com/sunchaser/chunyu/helloworld/controller/HelloController.java
+++ chunyu-hello-world/src/main/java/com/sunchaser/chunyu/helloworld/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * Hello Controller
+ *
  * @author sunchaser admin@lilu.org.cn
  * @since JDK8 2022/3/6
  */
