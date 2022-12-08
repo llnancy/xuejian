ad1440bb1a9cbd71a8b05aea140c7af804704437
SunChaser <40930677+llnancy@users.noreply.github.com>
2022-12-08 21:36:17 +0800

    Feature/mybatis plus (#9)
    * feat: Update chunyu-mybatisplus module
    
    * feat: fix jackson config
    
    * feat: fix jackson config

--- chunyu-mybatis-plus/src/main/resources/scripts/schema.sql
+++ chunyu-mybatis-plus/src/main/resources/scripts/schema.sql
@@ -8,8 +8,12 @@ CREATE TABLE `chunyu_mp`.`mp_user`
     `name`        varchar(18)          NOT NULL DEFAULT '无名氏' COMMENT '姓名',
     `age`         tinyint(3) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '年龄',
     `address`     varchar(64)          NOT NULL COMMENT '地址',
+    `create_user` varchar(64)          NOT NULL COMMENT '创建人',
     `create_time` datetime             NOT NULL COMMENT '创建时间',
-    `update_time` datetime             NOT NULL COMMENT '修改时间',
+    `update_user` varchar(64)          NOT NULL COMMENT '更新人',
+    `update_time` datetime             NOT NULL COMMENT '更新时间',
     `is_deleted`  tinyint(1) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '状态（0：正常；1：删除）',
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'Mybatis-Plus 用户表';
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = 'Mybatis-Plus 用户表';
