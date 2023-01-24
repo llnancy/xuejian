81d1e69797bb0ceba24d7aafa68320e17f6336b7
lilu <admin@lilu.org.cn>
2023-01-24 22:07:08 +0800

    feat: add unit test

--- xuejian-webflux/src/test/java/io/github/llnancy/xuejian/webflux/test/StepVerifierTest.java
+++ xuejian-webflux/src/test/java/io/github/llnancy/xuejian/webflux/test/StepVerifierTest.java
@@ -7,17 +7,23 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 /**
- * test {@link StepVerifier}
+ * {@link StepVerifier} test
  *
  * @author sunchaser admin@lilu.org.cn
  * @since JDK8 2023/1/12
  */
 @SpringBootTest
-public class StepVerifierTest {
+class StepVerifierTest {
 
     @Test
     public void test() {
         Flux<String> flux = Flux.just("hello", "webflux");
+
+        // 初始化：将 Flux/Mono 数据流传入 StepVerifier 的 create 方法
+        // 正常数据流断言：expectNext/expectNextMatches/assertNext
+        // 完成数据流断言：expectComplete
+        // 异常数据流断言：expectError/expectErrorMessage
+        // 启动测试：verify 方法
 
         // expectNext
         StepVerifier.create(flux)
